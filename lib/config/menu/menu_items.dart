8c12415ff3a6e053082fd161c45bce485ac85dfb
Hugo Grados <hdgch1106@gmail.com>
2023-07-06 12:59:47 -0500

    Imagenes agregadas

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -43,5 +43,10 @@ const appMenuItems = <MenuItem>[
       title: "UI controls + Tiles",
       subTitle: "Una serie de controles de flutter",
       link: "/ui-controls",
-      icon: Icons.car_rental_outlined)
+      icon: Icons.car_rental_outlined),
+  MenuItem(
+      title: "Introducción a la aplicación",
+      subTitle: "Pequeño tutorial introductorio",
+      link: "/tutorial",
+      icon: Icons.accessible_rounded)
 ];
